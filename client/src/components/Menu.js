250ec38aa8ee2a8755a4e1c4af34e1315c2e410f
unknown <igorpopach@gmail.com>
2019-01-08 16:40:29 +0200

    update menu component

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -3,7 +3,13 @@ import React, { Component } from "react";
 export default class Menu extends Component {
     state = {
         dishesData: [],
+        saladsCat: [],
+        FirstDishesCat: [],
+        HotDishesCat: [],
+        DessertsCat: [],
+        AlcoholDrinksCat: [],
         intervalIsSet: false,
+        isToggleOn: false,
     };
     componentDidMount() {
         this.getDataFromDb();
@@ -28,103 +34,212 @@ export default class Menu extends Component {
     getDataFromDb = () => {
         fetch("/api/Dishes")
             .then(data => data.json())
-            .then(res => this.setState({ dishesData: res.dishesData }));
-        };
+            .then(res => this.setState({ dishesData: res.dishesData }))
+            .then(res => this.categoryFilter(this.state.dishesData))
+    };
+    
+    categoryFilter = (data) => {
+        let saladsCat = []
+        saladsCat = data.filter(item => {
+            if (item.category === "Salads") {
+                return item
+            }
+            return false
+        })
+        let FirstDishesCat = []
+        FirstDishesCat = data.filter(item => {
+            if (item.category === "First dishes") {
+                return item
+            }
+            return false
+        })
+        let HotDishesCat = []
+        HotDishesCat = data.filter(item => {
+            if (item.category === "Hot dishes") {
+                return item
+            }
+            return false
+        })
+        let DessertsCat = []
+        DessertsCat = data.filter(item => {
+            if (item.category === "Desserts") {
+                return item
+            }
+            return false
+        })
+        let AlcoholDrinksCat = []
+        AlcoholDrinksCat = data.filter(item => {
+            if (item.category === "Alcohol drinks") {
+                return item
+            }
+            return false
+        })
+        this.setState({
+            saladsCat: saladsCat,
+            FirstDishesCat: FirstDishesCat,
+            HotDishesCat: HotDishesCat,
+            DessertsCat: DessertsCat,
+            AlcoholDrinksCat: AlcoholDrinksCat,
+        })
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+
     
     render(){
-        const data = this.state.dishesData
-        console.log('data',data)
+        const {saladsCat, FirstDishesCat, HotDishesCat, DessertsCat, AlcoholDrinksCat} = this.state
         return (
-            <div>
-                <h2>Salads:</h2>
-                <ul>
-                    {data.length <= 0
-                        ? "NO DB DISHES ENTRIES YET"
-                    : data.map(dat => {
-                            if(dat.category === "Salads")
-                                return <div key={dat._id}>
-                                            <li style={{ padding: "10px", display: "inline-block" }}>
-                                                <p>{dat.name}</p>
-                                                <span style={{ color: "gray" }}> (ingredients: </span> {dat.ingredients})
-                                                <span style={{ color: "gray" }}> weight: </span> {dat.weight}
-                                                <span style={{ color: "gray" }}> price: </span> {dat.price}
-                                            </li> 
+            <div style={{ backgroundColor: "#cce6ff"}}>
+                <div className="row">
+                    <div className="col-6">
+                        <h2>Salads:</h2>
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        weight, g
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        price, $
+                    </div>
+                </div>
+                {saladsCat.length <= 0
+                    ? "NO DB DISHES ENTRIES YET"
+                    : saladsCat.map(dat => {
+                            return <div key={dat._id}>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                {dat.name}
+                                                <button onClick={this.handleClick}>
+                                                    {this.state.isToggleOn ? `Don't order` : 'Order'}
+                                                </button>
+                                            </div>
+                                            <div className="col-3">{dat.weight}</div>
+                                            <div className="col-3">{dat.price}</div>
                                         </div>
-                        })
-                    }
-                </ul>
-                <h2>First dishes:</h2>
-                <ul>
-                    {data.length <= 0
-                        ? "NO DB DISHES ENTRIES YET"
-                        : data.map(dat => {
-                            console.log('dat.category', dat.category )
-                            if(dat.category === "First dishes")
-                                return <div key={dat._id}>
-                                            <li style={{ padding: "10px", display: "inline-block" }}>
-                                                <p>{dat.name}</p>
-                                                <span style={{ color: "gray" }}> (ingredients: </span> {dat.ingredients})
-                                                <span style={{ color: "gray" }}> weight: </span> {dat.weight}
-                                                <span style={{ color: "gray" }}> price: </span> {dat.price}
-                                            </li> 
+                                        <p>(ingredients:  {dat.ingredients})</p>
+                                    </div>
+                    })
+                }
+                <div className="row">
+                    <div className="col-6">
+                    <h2>First dishes:</h2>
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        weight, g
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        price, $
+                    </div>
+                </div>
+                {FirstDishesCat.length <= 0
+                    ? "NO DB DISHES ENTRIES YET"
+                    : saladsCat.map(dat => {
+                            return <div key={dat._id}>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                {dat.name}
+                                                <button onClick={this.handleClick}>
+                                                    {this.state.isToggleOn ? `Don't order` : 'Order'}
+                                                </button>
+                                            </div>
+                                            <div className="col-3">{dat.weight}</div>
+                                            <div className="col-3">{dat.price}</div>
                                         </div>
-                        })
-                    }
-                </ul>
-                <h2>Hot dishes:</h2>
-                <ul>
-                    {data.length <= 0
-                        ? "NO DB DISHES ENTRIES YET"
-                        : data.map(dat => {
-                            console.log('dat.category', dat.category )
-                            if(dat.category === "Hot dishes")
-                                return <div key={dat._id}>
-                                            <li style={{ padding: "10px", display: "inline-block" }}>
-                                                <p>{dat.name}</p>
-                                                <span style={{ color: "gray" }}> (ingredients: </span> {dat.ingredients})
-                                                <span style={{ color: "gray" }}> weight: </span> {dat.weight}
-                                                <span style={{ color: "gray" }}> price: </span> {dat.price}
-                                            </li> 
+                                        <p>(ingredients:  {dat.ingredients})</p>
+                                    </div>
+                    })
+                }
+                <div className="row">
+                    <div className="col-6">
+                    <h2>Hot dishes:</h2>
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        weight, g
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        price, $
+                    </div>
+                </div>
+                {HotDishesCat.length <= 0
+                    ? "NO DB DISHES ENTRIES YET"
+                    : HotDishesCat.map(dat => {
+                            return <div key={dat._id}>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                {dat.name}
+                                                <button onClick={this.handleClick}>
+                                                    {this.state.isToggleOn ? `Don't order` : 'Order'}
+                                                </button>
+                                            </div>
+                                            <div className="col-3">{dat.weight}</div>
+                                            <div className="col-3">{dat.price}</div>
                                         </div>
-                        })
-                    }
-                </ul>
-                <h2>Desserts:</h2>
-                <ul>
-                    {data.length <= 0
-                        ? "NO DB DISHES ENTRIES YET"
-                        : data.map(dat => {
-                            console.log('dat.category', dat.category )
-                            if(dat.category === "Hot dishes")
-                                return <div key={dat._id}>
-                                            <li style={{ padding: "10px", display: "inline-block" }}>
-                                                <p>{dat.name}</p>
-                                                <span style={{ color: "gray" }}> (ingredients: </span> {dat.ingredients})
-                                                <span style={{ color: "gray" }}> weight: </span> {dat.weight}
-                                                <span style={{ color: "gray" }}> price: </span> {dat.price}
-                                            </li> 
+                                        <p>(ingredients:  {dat.ingredients})</p>
+                                    </div>
+                    })
+                }
+                <div className="row">
+                    <div className="col-6">
+                    <h2>Desserts:</h2>
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        weight, g
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        price, $
+                    </div>
+                </div>
+                {DessertsCat.length <= 0
+                    ? "NO DB DISHES ENTRIES YET"
+                    : DessertsCat.map(dat => {
+                            return <div key={dat._id}>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                {dat.name}
+                                                <button onClick={this.handleClick}>
+                                                    {this.state.isToggleOn ? `Don't order` : 'Order'}
+                                                </button>
+                                            </div>
+                                            <div className="col-3">{dat.weight}</div>
+                                            <div className="col-3">{dat.price}</div>
                                         </div>
-                        })
-                    }
-                </ul>
-                <h2>Alcohol drinks:</h2>
-                <ul>
-                    {data.length <= 0
-                        ? "NO DB DISHES ENTRIES YET"
-                        : data.map(dat => {
-                            console.log('dat.category', dat.category )
-                            if(dat.category === "Hot dishes")
-                                return <div key={dat._id}>
-                                            <li style={{ padding: "10px", display: "inline-block" }}>
-                                                <p>{dat.name}</p>
-                                                <span style={{ color: "gray" }}> (ingredients: </span> {dat.ingredients})
-                                                <span style={{ color: "gray" }}> weight: </span> {dat.weight}
-                                                <span style={{ color: "gray" }}> price: </span> {dat.price}
-                                            </li> 
+                                        <p>(ingredients:  {dat.ingredients})</p>
+                                    </div>
+                    })
+                }
+                <div className="row">
+                    <div className="col-6">
+                    <h2>Alcohol drinks:</h2>
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        weight, g
+                    </div>
+                    <div className="col-3"style={{ lineHeight: "40px"}}>
+                        price, $
+                    </div>
+                </div>
+                {AlcoholDrinksCat.length <= 0
+                    ? "NO DB DISHES ENTRIES YET"
+                    : AlcoholDrinksCat.map(dat => {
+                            return <div key={dat._id}>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                {dat.name}
+                                                <button onClick={this.handleClick}>
+                                                    {this.state.isToggleOn ? `Don't order` : 'Order'}
+                                                </button>
+                                            </div>
+                                            <div className="col-3">{dat.weight}</div>
+                                            <div className="col-3">{dat.price}</div>
                                         </div>
-                        })
-                    }
-                </ul>
+                                        <p>(ingredients:  {dat.ingredients})</p>
+                                    </div>
+                    })
+                }
             </div>
         )
     }
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,8 +1,100 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class Table extends Component {
+    state = {
+        data: [],
+        dishesData: [],
+        id: 0,
+        message: null,
+        status: null,
+        intervalIsSet: false,
+        idToDelete: null,
+        idToUpdate: null,
+        objectToUpdate: null
+    };
+
+    // when component mounts, first thing it does is fetch all existing data in our db
+    // then we incorporate a polling logic so that we can easily see if our db has 
+    // changed and implement those changes into our UI
+
+    componentDidMount() {
+        this.getDataFromDb();
+        if (!this.state.intervalIsSet) {
+        let interval = setInterval(this.getDataFromDb, 88000);
+        this.setState({ intervalIsSet: interval });
+        }
+    }
+
+    // never let a process live forever 
+    // always kill a process everytime we are done using it
+
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+        clearInterval(this.state.intervalIsSet);
+        this.setState({ intervalIsSet: null });
+        }
+    }
+
+    // our first get method that uses our backend api to 
+    // fetch data from our data base
+    getDataFromDb = () => {
+        fetch("/api/Table")
+        .then(data => data.json())
+        .then(res => this.setState({ data: res.data }));
+    };
+
+
+    // our delete method that uses our backend api 
+    // to remove existing database information
+    deleteFromDB = idTodelete => {
+        let objIdToDelete = null;
+        this.state.data.forEach(dat => {
+        if (dat.id == idTodelete) {
+            objIdToDelete = dat._id;
+        }
+        });
+
+        axios.delete("/api/Table/delete", {
+        data: {
+            _id: objIdToDelete
+        }
+        });
+    };
+
+
+    // our update method that uses our backend api
+    // to overwrite existing data base information
+    updateDB = (idToUpdate, updateToApply) => {
+        let objIdToUpdate = null;
+        this.state.data.forEach(dat => {
+        if (dat.id == idToUpdate) {
+            objIdToUpdate = dat._id;
+        }
+        });
+        axios.post("/api/Table/update", {
+        id: objIdToUpdate,
+        update: { status: updateToApply }
+        });
+    };
+
+    // our put method that uses our backend api
+    // to create new query into our data base
+    putStatusToDB = data => {
+        console.log('data',data)
+        let currentIds = this.state.data.map(data => data.id);
+        let idToBeAdded = 0;
+        while (currentIds.includes(idToBeAdded)) {
+        ++idToBeAdded;
+        }
+        axios.post("/api/Table/add", {
+        id: idToBeAdded,
+        tableName: data.tableName,
+        status: data.status
+        });
+    };
     render(){
-        const data = this.props.data
+        const data = this.state.data
         return (
             <div>
                 <ul>
@@ -23,7 +115,7 @@ export default class Table extends Component {
                         onChange={e => this.setState({ idToDelete: e.target.value })}
                         placeholder="put id of item to delete here"
                     />
-                    <button onClick={() => this.props.deleteFromDB(this.state.idToDelete)}>
+                    <button onClick={() => this.deleteFromDB(this.state.idToDelete)}>
                         DELETE
                     </button>
                 </div>
@@ -42,7 +134,7 @@ export default class Table extends Component {
                     />
                     <button
                         onClick={() =>
-                        this.props.updateDB(this.state.idToUpdate, this.state.updateToApply)
+                        this.updateDB(this.state.idToUpdate, this.state.updateToApply)
                         }
                     >
                         UPDATE
@@ -61,7 +153,7 @@ export default class Table extends Component {
                         placeholder="add table number in the database"
                         style={{ width: "200px" }}
                     />
-                    <button onClick={() => this.props.putStatusToDB(this.state)}>
+                    <button onClick={() => this.putStatusToDB(this.state)}>
                         ADD
                     </button>
                 </div>
