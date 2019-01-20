c474a492e5df9a78a6c8d0bbb670411901cba427
unknown <igorpopach@gmail.com>
2019-01-20 22:10:06 +0200

    test

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
-
+    state = {
+        username: 'TEST-USER',
+        password: 'TEST-PASSWORD',
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        // axios.post('/api/auth/login', {
+        //     username: 'TEST-USER',
+        //     password: 'TEST-PASSWORD',
+        // }).then((user) =>  this.setState({ user }))
+        setTimeout(() => {
+            this.setState({ user: {
+                username,
+                password,
+            } })
+        }, 1000)
+    }
     render() {
+        const { username, password } = this.state;
         return (
             <div className="container">
-                <form class="form-signin" action="/login" method="post" style={{maxWidth: "300px"}}>
-                    <h2 class="form-signin-heading">Please sign in</h2>
-                    <label class="sr-only" for="inputEmail"></label>
-                    <input class="form-control" type="text" name="username" id="inputEmail" placeholder="Username" required autofocus />
-                    <input class="form-control" type="password" name="password" id="inputPassword" placeholder="Password" />
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">LOGIN</button>
+                <form className="form-signin" action="/auth/login" method="post" onSubmit={this.handleSubmit} style={{maxWidth: "300px"}}>
+                    <h2 className="form-signin-heading">Please sign in</h2>
+                    <label className="sr-only" for="inputEmail"></label>
+                    <input className="form-control" type="text" name="username" value={username} id="inputEmail" placeholder="Username" required autofocus />
+                    <input className="form-control" type="password" name="password" value={password} id="inputPassword" placeholder="Password" />
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">LOGIN</button>
                 </form>
             </div>
         )
