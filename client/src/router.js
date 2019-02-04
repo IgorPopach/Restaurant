57a682dfdecbb8bd1bd9d23de1d4574c827826cc
IgorPopach <37268094+IgorPopach@users.noreply.github.com>
2019-02-04 12:48:03 +0200

    Update router.js

--- client/src/router.js
+++ client/src/router.js
@@ -1,23 +1,28 @@
 import React from "react";
 import HomePage from "./containers/HomePage";
-import LogInPage from "./containers/LogIn";
+import LogInPage from "./containers/LogInPage";
 import RegisterPage from "./containers/RegisterPage";
 import MenuPage from "./containers/MenuPage";
 import EditBase from "./containers/EditBase";
 import ChefPage from "./containers/ChefPage";
 import { Route, Switch } from "react-router-dom";
+import ProfilePage from "./containers/ProfilePage";
 
 
-const Router = () => (
-    <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/login" component={LogInPage} />
-        <Route path="/register" component={RegisterPage} />
-        <Route path="/menu" component={MenuPage} />
-        <Route path="/edit-base" component={EditBase} />
-        <Route path="/chef-orders" component={ChefPage} />
-        <Route render={() => (<div style={{color: "white", textAlign: "center", fontSize: "2rem"}}>Page not found </div>)} />
-    </Switch>
-);
-
+class Router extends React.Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/login" component={LogInPage} />
+                <Route path="/register" component={RegisterPage} />
+                <Route path="/menu" component={MenuPage} />
+                <Route path="/edit-base" component={EditBase} />
+                <Route path="/chef-orders" component={ChefPage} />
+                <Route path="/profile" component={ProfilePage} />
+                <Route render={() => (<div style={{color: "white", textAlign: "center", fontSize: "2rem"}}>Page not found </div>)} />
+            </Switch>
+        );
+    }
+}
 export default Router;
